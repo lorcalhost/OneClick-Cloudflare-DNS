7b61b5d397f5604eae6b6ea0cc2854d9d043604d
Lorenzo Callegari <Callegari.lorenzo@gmail.com>
2019-05-04 17:27:50 +0200

    Performance imporvements

--- enable.bat
+++ enable.bat
@@ -1,5 +1,6 @@
 set /p ADAPTERNAME=<adapter_name.txt
-netsh interface ipv4 add dnsserver %ADAPTERNAME% 1.1.1.1 index=1
-netsh interface ipv4 add dnsserver %ADAPTERNAME% 1.0.0.1 index=2
-netsh interface ipv6 add dnsserver %ADAPTERNAME% 2606:4700:4700::1111 index=1
-netsh interface ipv6 add dnsserver %ADAPTERNAME% 2606:4700:4700::1001 index=2
+netsh interface ipv4 set dnsserver name=%ADAPTERNAME% static 1.1.1.1 primary validate=no
+netsh interface ipv4 add dnsserver %ADAPTERNAME% 1.0.0.1 index=2 validate=no
+netsh interface ipv6 set dnsserver name=%ADAPTERNAME% static 2606:4700:4700::1111 primary validate=no
+netsh interface ipv6 add dnsserver %ADAPTERNAME% 2606:4700:4700::1001 index=2 validate=no
+ipconfig /flushdns
