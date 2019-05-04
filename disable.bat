8be6133fc6856ca3e53ea3fb7d85c08b0c730de4
Lorenzo Callegari <Callegari.lorenzo@gmail.com>
2019-05-04 17:42:48 +0200

    Optimized for shortcuts

--- disable.bat
+++ disable.bat
@@ -1,4 +1,5 @@
-set /p ADAPTERNAME=<adapter_name.txt
+set scriptpath=%~dp0
+set /p ADAPTERNAME=<"%scriptpath%adapter_name.txt"
 netsh interface ipv4 set dns %ADAPTERNAME% dhcp
 netsh interface ipv6 set dns %ADAPTERNAME% dhcp
 ipconfig /flushdns
