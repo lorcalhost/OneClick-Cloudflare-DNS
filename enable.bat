e67e65459893ea5262d621f188f30cfcb95c9dce
Lorenzo Callegari <Callegari.lorenzo@gmail.com>
2019-05-04 17:42:19 +0200

    Optimized for shortcuts
    Can now run the script from a shortcut

--- enable.bat
+++ enable.bat
@@ -1,4 +1,5 @@
-set /p ADAPTERNAME=<adapter_name.txt
+set scriptpath=%~dp0
+set /p ADAPTERNAME=<"%scriptpath%adapter_name.txt"
 netsh interface ipv4 set dnsserver name=%ADAPTERNAME% static 1.1.1.1 primary validate=no
 netsh interface ipv4 add dnsserver %ADAPTERNAME% 1.0.0.1 index=2 validate=no
 netsh interface ipv6 set dnsserver name=%ADAPTERNAME% static 2606:4700:4700::1111 primary validate=no
